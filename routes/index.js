fab2b2b102485a332ea6100a2d3b6be4de0cb827
iteriani <fortrex@gmail.com>
2014-04-15 18:00:59 -0700

    twitter

--- routes/index.js
+++ routes/index.js
@@ -11,35 +11,24 @@ exports.view = function(req, res) {
 
 		twitter.getAccessToken(req.session.token, req.session.secret, req.query.oauth_verifier,
 			function(error, accessToken, accessTokenSecret, results){
-				console.log();
-				console.log(req.query, req.session, "SESSIONS");
-				console.log();
-				console.log(accessToken, accessTokenSecret, "ACCESSTOKENS");
-				console.log();
-				console.log(error, results, "RESULTS/ERROR");
-				console.log();
 
+				req.session.oauth_verifier = req.query.oauth_verifier;
 				req.session.accessToken = accessToken;
 				req.session.accessSecret = accessTokenSecret;
 				twitter.getTimeline("home", {}, req.session.accessToken, req.session.accessSecret, function(error, data, response){
 					console.log(error, data, response);
-					res.render("index", {tweets : data});
+					res.render("index", {tweets : JSON.stringify(data)});
 				});
 			});
 
-	}else{
-
-		console.log(req.session, "NEW SESSION")
-		console.log();
-		if(req.session.accessToken != null && req.session.accessSecret != null){
-			twitter.getTimeline("home", {}, req.session.accessToken, req.session.accessSecret, function(error, data, response){
-				res.render("index", {tweets : data});
-			});
+	}else if (req.session.accessToken != null && req.session.accessSecret != null){
+				twitter.getTimeline("home", {}, req.session.accessToken, req.session.accessSecret, function(error, data, response){
+					console.log(error, data, response);
+					res.render("index", {tweets : JSON.stringify(data)});
+				});
 		}else{
-			res.render("index", {tweets : "NONE"});
+			res.render("index", {tweets : "[]"});
 		}
-
-	}
 
 }
 
